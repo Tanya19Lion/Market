97deab9b5df8b03548a3418376da4de26663ee58
Tanya19Lion <tanyatriteknitskaya@gmail.com>
2020-02-12 18:29:07 +0200

    added slider section

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,17 @@
 $(function(){
 
+     $(".rate_star").rateYo({
+          rating: 5,
+          starWidth: "12px",
+          readOnly: true
+     });
+
      var mixer = mixitup('.products_inner_box');
+     $(".product_slider_inner").slick({
+          dots: true,
+          arrows: false,
+          slidesToShow: 4,
+          slidesToScroll: 4,
+      });
     
 });
